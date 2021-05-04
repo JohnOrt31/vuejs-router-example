63bec6ac6ec435ebe1f63f3338a3f931efc3a4b4
JohnOrt31 <jj.coronaortega@ugto.mx>
2021-05-04 13:11:45 -0500

    Consumiendo api

--- src/views/About.vue
+++ src/views/About.vue
@@ -10,7 +10,8 @@ import Titulo from '../components/Titulo'
 export default {
   components: {
     Titulo
-  }
+  },
+ 
 }
 </script>
 
--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,5 +1,9 @@
 <template>
     <Titulo texto="Aquí va un titulo generico"/>
+    <button @click="consumirAPI">Consumir la API</button>
+    <div v-for="item in arrayBlog" :key="item.id">
+        {{ item.title }}
+    </div>
 </template>
 
 <script>
@@ -7,7 +11,25 @@ import Titulo from '../components/Titulo'
 export default {
     components: {
         Titulo
-    }
+    },
+    data() {
+        return {
+            arrayBlog: []
+        }
+  },
+    methods: {
+        async consumirAPI(){
+            try {
+                const data = await fetch('https://jsonplaceholder.typicode.com/posts');
+                const array = await data.json();
+                console.log(array);
+                this.arrayBlog = array;
+
+            } catch (error) {
+                console.log(error);
+            }
+        }   
+    },
 
 }
 </script>
